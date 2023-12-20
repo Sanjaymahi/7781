4a90893b0f79edac9af2bf37d1a3654daf250018
sanjumahi <gaddamsanjai545@gmail.com>
2023-12-20 04:46:28 +0000

    vinay

---
+++ jay/src/main/java/san/jay/practice.java
@@ -0,0 +1,14 @@
+package san.jay;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 5;
+    int b = 6;
+    int result =(a+b);
+    System.out.println(result);
+    
+	}
+
+}
